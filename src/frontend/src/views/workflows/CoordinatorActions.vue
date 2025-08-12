00d05b20417235fc14b161dee1b161c937e5e46f
robot-mazeee <madalenacmo@gmail.com>
2025-08-12 15:47:51 +0100

    undo button in juri selection

--- src/frontend/src/views/workflows/CoordinatorActions.vue
+++ src/frontend/src/views/workflows/CoordinatorActions.vue
@@ -1,15 +1,25 @@
 <template>
-    <SelectJuriPresidentDialog 
-        v-if="props.workflow.status === 'APPROVED_BY_ADMIN'" 
-        :workflow="props.workflow" 
-    />
+    <SelectJuriPresidentDialog v-if="props.workflow.status === 'APPROVED_BY_ADMIN'" :workflow="props.workflow" />
+    <v-btn 
+        v-else-if="props.workflow.status === 'JURI_PRESIDENT_ATTRIBUTED'"
+        @click="changeStatus('APPROVED_BY_ADMIN')"
+        class="mb-3" 
+        color="error"
+    >Undo</v-btn>
 </template>
 
 <script setup lang="ts">
 import WorkflowDto from '../../models/WorkflowDto';
+import { useWorkflowStore } from '../../stores/workflows';
 import SelectJuriPresidentDialog from '../dialogs/SelectJuriPresidentDialog.vue';
 
 const props = defineProps<{
     workflow: WorkflowDto
 }>();
+
+const workflowStore = useWorkflowStore();
+
+function changeStatus(status: string) {
+    workflowStore.updateStatus(props.workflow, status);
+}
 </script>
