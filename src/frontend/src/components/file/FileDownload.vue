d3f96ec99b9c697826f36145cb232075444d5fc5
robot-mazeee <madalenacmo@gmail.com>
2025-08-17 15:48:21 +0100

    file download vue component

---
+++ src/frontend/src/components/file/FileDownload.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-btn 
+        @click="downloadPdf"
+        class="mb-3" 
+        color="warning"
+    >Download PDF</v-btn>
+</template>
+
+<script setup lang="ts">
+import WorkflowDto from '../../models/WorkflowDto';
+import RemoteServices from '../../services/RemoteService';
+
+const props = defineProps<{
+    workflow: WorkflowDto
+}>();
+
+async function downloadPdf() {
+    try {
+        await RemoteServices.downloadWorkflowPdf(props.workflow.id);
+    } catch (error) {
+        console.error("Failed to download PDF", error);
+    }
+}
+</script>
